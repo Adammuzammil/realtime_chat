5f474d205e4540d472a5d8b6669836568bb75eb6
Adam Mohammed Muzammil <mm592678@gmail.com>
2026-01-28 22:00:04 +0530

    Add dependencies and implement chat room functionality

---
+++ src/app/api/[[...slugs]]/route.ts
@@ -0,0 +1,28 @@
+import { redis } from "@/lib/redis";
+import { Elysia, t } from "elysia";
+import { nanoid } from "nanoid";
+
+const ROOM_TTL_SECONDS = 60 * 10;
+
+const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
+  const roomId = nanoid();
+
+  //Connect to redis db
+  //hset - used for storing objects with multiple fields
+  await redis.hset(`meta:${roomId}`, {
+    connected: [],
+    createdAt: Date.now(),
+  });
+
+  //Destroy the chat room after x mins/secs
+  await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS);
+
+  return { roomId };
+});
+
+const app = new Elysia({ prefix: "/api" }).use(rooms);
+
+export const GET = app.fetch;
+export const POST = app.fetch;
+
+export type App = typeof app;
